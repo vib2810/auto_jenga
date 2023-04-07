c6061a56b2798e1a8bcb6423df8e9bfeee7114f4
Dhanvi Sreenivasan <dhanvi.sreeni@gmail.com>
2023-04-07 18:31:27 -0400

    Updated block pose planner with service-response code

--- src/jenga_packages/block_pose_planner/src/test_block_move.py
+++ src/jenga_packages/block_pose_planner/src/test_block_move.py
@@ -1,60 +1,77 @@
 #!/usr/bin/env python
 
 import rospy
-import tf
+# import tf
 import numpy as np
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseStamped
+from block_pose_planner.srv import *
 from frankapy import FrankaArm
+import quaternion
+from autolab_core import RigidTransform
 
 # 75mm length, 15mm height
 
-class GripperPoseCalculatorNode(object):
-    def __init__(self):
-        # Initialize node
-        rospy.init_node('gripper_pose_calculator')
+def pickup_block(req_pose):
+    global fa
+    np_quat = quaternion.as_quat_array(np.array([req_pose.pose.orientation.w, req_pose.pose.orientation.x, req_pose.pose.orientation.y, req_pose.pose.orientation.z]))
+    transl = np.array([req_pose.pose.position.x, req_pose.pose.position.y, req_pose.pose.position.z + 0.1])
+    local_transl = np.array([req_pose.pose.position.x, req_pose.pose.position.y, req_pose.pose.position.z])
+    pose_to_set = RigidTransform(rotation=quaternion.as_rotation_matrix(np_quat), translation = transl, from_frame="franka_tool", to_frame="world")
+    fa.goto_pose(pose_to_set, use_impedance=False, duration=7)
+    local_pose_to_set = RigidTransform(rotation=quaternion.as_rotation_matrix(np_quat), translation = local_transl, from_frame="franka_tool", to_frame="world")
+    fa.goto_pose(local_pose_to_set, use_impedance=False, duration=5)
+    fa.close_gripper()
+    fa.reset_pose(duration=10)
 
-        # Subscribe to pose topic
-        self.pose_sub = rospy.Subscriber('pose_topic', Pose, self.pose_callback)
+    return True
 
-        # Initialize FrankaArm object
-        self.arm = FrankaArm()
+def drop_block(block_id):
+    global fa
 
-        # Initialize variables
-        self.gripper_pose = None
+    # Hard coded because staging area - need to update when testing with actual limits
+    middle_block_pose ={'orientation': np.array([0.020, 0.9985, 0.041, 0.0288]), 
+                        'translation': np.array([0.487, -0.08, 0.107])}
+    
+    drop_pose_quat = quaternion.as_quat_array(middle_block_pose['orientation'])
+    drop_pose_transl = middle_block_pose['translation'] + (1 - block_id%3)*np.array([0.025, 0, 0])
+    pose_to_drop = RigidTransform(rotation=quaternion.as_rotation_matrix(drop_pose_quat), translation = drop_pose_transl, from_frame="franka_tool", to_frame="world")
+    fa.goto_pose(pose_to_drop, use_impedance=False, duration=15)
+    drop_pose_local_transl = middle_block_pose['translation'] + (1 - block_id%3)*np.array([0.025, 0, 0]) - np.array([0, 0, 0.1])
+    pose_to_drop_local = RigidTransform(rotation=quaternion.as_rotation_matrix(drop_pose_quat), translation = drop_pose_local_transl, from_frame="franka_tool", to_frame="world")
+    fa.goto_pose(pose_to_drop_local, use_impedance=False, duration=10)
+    fa.open_gripper()
+    fa.reset_pose(duration=15)
 
-        # Spin node
-        rospy.spin()
+    return True
+    
 
-    def pose_callback(self, msg):
-        # Convert pose message to a transformation matrix
-        pose = tf.transformations.translation_matrix((msg.position.x, msg.position.y, msg.position.z))
-        quaternion = (
-            msg.orientation.x,
-            msg.orientation.y,
-            msg.orientation.z,
-            msg.orientation.w
-        )
-        rotation = tf.transformations.quaternion_matrix(quaternion)
-        transform = rotation.dot(pose)
 
-        # Define gripper pose offset from end effector
-        offset = tf.transformations.translation_matrix((0, 0, -0.135))
+def recv_pose_callback(req):
+    global fa
+    print("Received pose information")
+    req_pose = req.des_pose
+    pickup_done = pickup_block(req_pose)
+    if(pickup_done):
+        drop_block(req.id)
 
-        # Calculate gripper pose
-        self.gripper_pose = transform.dot(offset)
+    return block_poseResponse(True)
 
-        # Move to start pose
-        self.arm.goto_pose(tool_pose = self.gripper_pose.flatten().tolist(), duration=1, dynamic=True)
-        rospy.sleep(1.5)
-        
-        # Close gripper to grip block
-        self.arm.close_gripper()
-        
-        # rospy.sleep(0.5)  # Wait for gripper to close
+def GoToBlockServer():
+    s = rospy.Service('go_to_block', block_pose, recv_pose_callback)
+    rospy.spin()
 
-        # Open gripper to release block
-        # self.arm.set_gripper_width(gripper_width)
-        # rospy.sleep(0.5)  # Wait for gripper to open
-        
 if __name__ == '__main__':
-    GripperPoseCalculatorNode()
+    global fa
+
+    rospy.init_node('block_pose_planner')
+    fa = FrankaArm(init_node=False)
+    GoToBlockServer()
+    
+    # des_pose = RigidTransform(rotation=np.eye(3), translation=np.array([0.4, 0, 0.165]), from_frame="franka_tool", to_frame="world")
+    # current_pose = fa.get_pose()
+    # des_pose = RigidTransform(rotation = np.array([[ 0.99735518,  0.00172061 , 0.07252959], [-0.00637742, -0.99375986,  0.11127286], [ 0.07226846, -0.11144111, -0.99113965]]),
+    #                         #   translation= np.array([ 0.45871361 ,-0.03142103,  0.0412673 ]), 
+    #                           from_frame='franka_tool', to_frame='world')
+    # print(current_pose)
+    # fa.goto_pose(des_pose, use_impedance=False, duration = 10)
+    
